cd39236132a638ce3c4a3504a644274848ffb192
sdi <sdi@9777c72b-191b-4fc1-b27c-20aeb54a8483>
2012-11-14 14:24:01 +0000

    DIFI-2354 Test av validate WellFormed

---
+++ validate-lib/src/test/java/no/difi/vefa/validation/WellFormedTest.java
@@ -0,0 +1,30 @@
+package no.difi.vefa.validation;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import no.difi.vefa.message.Message;
+import no.difi.vefa.message.MessageType;
+import no.difi.vefa.message.ValidationType;
+
+import org.junit.Test;
+
+public class WellFormedTest {
+	
+	@Test
+	public void testMain() throws Exception {		
+		List<Message> messages = new ArrayList<Message>();
+		WellFormed wellFormed = new WellFormed();
+		
+		wellFormed.main("<data><test>Hello world</test></data>", messages);		
+		assertEquals(0, messages.size());
+		
+		wellFormed.main("<data><test>Hello world</test></data>error", messages);		
+		assertEquals(1, messages.size());
+		assertEquals(MessageType.Fatal, messages.get(0).messageType);
+		assertEquals(ValidationType.XMLWellFormed, messages.get(0).validationType);
+	}
+
+}
