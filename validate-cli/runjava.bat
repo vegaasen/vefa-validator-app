395c93a8d99dd68b89cb8735e970fc9ff546f86a
sdi <sdi@9777c72b-191b-4fc1-b27c-20aeb54a8483>
2012-12-20 11:26:09 +0000

    DIFI-2354 Kan køyre automatisk og manuel validering i cli mode

--- validate-cli/runjava.bat
+++ validate-cli/runjava.bat
@@ -1,6 +1,15 @@
 @REM ----------------------------------------------------------------------------
-@REM Usage: runjava.bat pathToXmlFile
-@REM Example: runjava.bat c:\temp\invoice.xml
+@REM It is possible to run the application in 2 modes
+@REM Mode 1: automatic detect what version and id from configuration to use when
+@REM 		 validating XML
+@REM 
+@REM Mode 2: spesify both version and id
+@REM 
+@REM Usage mode 1: runjava.bat pathToXmlFile
+@REM Example mode 1: runjava.bat c:\temp\invoice.xml
+@REM 
+@REM Usage mode 2: runjava.bat pathToXmlFile version id
+@REM Example mode 2: runjava.bat c:\temp\invoice.xml 1.6 urn:www.cenbii.eu:profile:bii04:ver1.0#urn:www.cenbii.eu:transaction:biicoretrdm010:ver1.0#urn:www.peppol.eu:bis:peppol4a:ver1.0#urn:www.difi.no:ehf:faktura:ver1
 @REM 
 @REM ----------------------------------------------------------------------------
 
--- validate-cli/src/main/java/no/difi/vefa/cli/App.java
+++ validate-cli/src/main/java/no/difi/vefa/cli/App.java
@@ -18,12 +18,32 @@ public class App
 	 */
     public static void main( String[] args ) throws Exception
     {
+    	if ((args == null || args.length == 0)) {
+    		System.out.println("No XML file is spesified!");
+    		System.exit(0);	
+    	}
+
 		String file = args[0];
 		String xml = new Scanner(new File(file)).useDelimiter("\\Z").next();
+
 		Validate validate = new Validate();
-		validate.autodetectVersionAndIdentifier = true;
-		validate.xml = xml;
-		validate.main();
-		System.out.println(validate.messagesAsXML());
+		
+		if (args.length == 1) {			
+			validate.autodetectVersionAndIdentifier = true;
+			validate.xml = xml;
+			validate.main();
+			System.out.println(validate.messagesAsXML());			
+		}
+		
+		if (args.length == 3) {
+			String version = args[1];
+			String id = args[2];			
+			
+			validate.xml = xml;
+			validate.version = version;
+			validate.id = id;
+			validate.main();
+			System.out.println(validate.messagesAsXML());			
+		}		
     }
 }
